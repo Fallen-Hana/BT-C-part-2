65a01041f55f229a0e3221cbf2306b13b4c8d7fc
Fallen Hana <94212686+Fallen-Hana@users.noreply.github.com>
2022-03-26 23:51:38 +0700

    Add files via upload

--- B2_C5.c
+++ B2_C5.c
@@ -1,56 +1,37 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#define max 10
+#include <stdio.h>
+#include <math.h>
 
-
-int main() {
-	int i,j = 0;
-	int n;
-	printf("nhap so phan tu cua day so: ");
-	scanf("%d", &n);
-	fflush(stdin);
-	int arr[n];
-	char dayso[n+n-1];
-	gets(dayso);
-	char * p;
-	p = strtok(dayso, ",");
-	arr[i] = atoi(p);
-	i++;
-//	printf("%d\n", arr[0]);
-	while (p != NULL) {
-		p = strtok(NULL, ",");
-		if(p != NULL) {
-//			printf("%s\n", p);
-			arr[i] = atoi(p);
-			i++;
-		}
-	}
-	int dem_so_duong = 0;
-	int dem_so_am = 0;
-	float tong_duong = 0;
-	float tong_am = 0;
-	for(j = 0;j < n; j++) {
-//		printf("%d", arr[j]);
-		
-		if(arr[j] > 0) {
-			dem_so_duong++;
-			tong_duong = tong_duong + arr[j];	
-		}else if (arr[j] < 0) {
-			dem_so_am++;
-			tong_am = tong_am + arr[j];
-		}else {
-			continue;
-		}
-		
-	}
-	if (dem_so_am == 0 ) {
-		dem_so_am = 1;
-	}
-	if (dem_so_duong == 0 ) {
-		dem_so_duong = 1;
-	}
-	printf("\nTrung binh cong so duong la: %f", (tong_duong / dem_so_duong));
-	printf("\nTrung binh cong so am la: %f", (tong_am / dem_so_am));
-	return 0;
+int main()
+{
+    int n;
+    int a[100];
+    float A, B;
+    int dem1 = 0;
+    int Sa = 0;
+    int dem2 = 0;
+    int Sd = 0;
+    printf("nhap n= ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", a[i]);
+        if (a[i] < 0)
+        {
+            dem1++;
+            Sa += a[i];
+            A = Sa * 1.0 / dem1;
+        }
+        else
+        {
+            dem2++;
+            Sd += a[i];
+            B = Sd * 1.0 / dem2;
+        }
+    }
+    printf("\ntrung binh cong cua so duong la: %f\n", B);
+    printf("trung binh cong cua so am la %f", A);
 }
